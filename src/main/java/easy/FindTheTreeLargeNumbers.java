44347771421a0da56edd936ce086e14644cb663f
Ricardo Gomes <ipsiart@gmail.com>
2020-10-30 11:07:20 +0000

    LargestNumbers

---
+++ src/main/java/easy/FindTheTreeLargeNumbers.java
@@ -0,0 +1,16 @@
+package easy;
+
+import java.util.Arrays;
+
+public class FindTheTreeLargeNumbers {
+    public static int[] findThreeLargestNumbers(int[] array) {
+        // Write your code here.
+
+        return Arrays.stream(array).boxed()
+                .sorted( (a, b) -> - a.compareTo( b ) )
+                .limit( 3 )
+                .sorted()
+                .mapToInt(i -> i)
+                .toArray();
+    }
+}
