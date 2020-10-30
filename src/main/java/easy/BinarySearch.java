2b5f379b4f893a2354d246052cd7d59af23fab35
Ricardo Gomes <ipsiart@gmail.com>
2020-10-30 10:39:00 +0000

    BinarySearch

---
+++ src/main/java/easy/BinarySearch.java
@@ -0,0 +1,28 @@
+package easy;
+
+public class BinarySearch {
+    public static int binarySearch(int[] array, int target) {
+        // Write your code here.
+        return binarySearch( array, target, 0, array.length - 1 );
+    }
+
+    private static int binarySearch(final int[] array,
+                                    final int target,
+                                    final int start,
+                                    final int end) {
+        if (start > end) {
+            return -1;
+        } else {
+            int middle = (start + end) / 2;
+            int value = array[middle];
+
+            if (value == target) {
+                return middle;
+            } else {
+                return value > target
+                        ? binarySearch( array, target, start, middle - 1 )
+                        : binarySearch( array, target, middle + 1, end );
+            }
+        }
+    }
+}
