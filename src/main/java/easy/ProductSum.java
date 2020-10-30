6ea357743b7f1c8f153757d7831eeaf92a822394
Ricardo Gomes <ipsiart@gmail.com>
2020-10-30 10:16:50 +0000

    ProductSum

---
+++ src/main/java/easy/ProductSum.java
@@ -0,0 +1,32 @@
+package easy;
+
+import com.sun.corba.se.spi.ior.ObjectKey;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductSum {
+    public static int productSum(List<Object> array) {
+        // Write your code here.
+        return productSum( array, 1, 0);
+    }
+
+    @SuppressWarnings (value="unchecked")
+    private static int productSum(final List<Object> array,
+                                  final int level,
+                                  final int index) {
+        if (index >= array.size()) {
+            return 0;
+        } else {
+            Object value = array.get( index );
+
+            if (value instanceof List) {
+                List<Object> listValue = (List<Object>) value;
+                int newValue = productSum( listValue, level + 1, 0 );
+                return (level + 1) * newValue + productSum( array, level, index + 1 );
+            } else {
+                return (int) value + productSum( array, level, index + 1 );
+            }
+        }
+    }
+}
---
+++ src/test/java/easy/ProductSumTest.java
@@ -0,0 +1,22 @@
+package easy;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.*;
+
+public class ProductSumTest {
+
+    @Test
+    public void productSum() {
+        List<Object> array = Stream.of(
+            5, 2, Stream.of( 7, -1 ).collect( Collectors.toList() ), 3,
+                Stream.of( 6, Stream.of( -13, 8 ).collect( Collectors.toList() ), 4).collect( Collectors.toList() )
+        ).collect( Collectors.toList());
+
+        ProductSum.productSum( array );
+    }
+}
