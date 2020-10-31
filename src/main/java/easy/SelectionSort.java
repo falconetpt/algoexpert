bef3c35e0a7ac54e8624a42a51e75a1086d4db6c
Ricardo Gomes <ipsiart@gmail.com>
2020-10-31 15:49:36 +0000

    Selection Sort

---
+++ src/main/java/easy/SelectionSort.java
@@ -0,0 +1,27 @@
+package easy;
+
+import java.util.stream.IntStream;
+
+public class SelectionSort {
+    public static int[] selectionSort(int[] array) {
+        // Write your code here.
+        selectionSort( array, 0 );
+        return array;
+    }
+
+    private static void selectionSort(final int[] array,
+                                      final int index) {
+        if (index < array.length) {
+            int minIndex = IntStream.range( index, array.length ).boxed()
+                    .reduce( index, (a, b) -> array[a] > array[b] ? b : a );
+
+            int tmp = array[index];
+            int minimum = array[minIndex];
+
+            array[index] = minimum;
+            array[minIndex] = tmp;
+
+            selectionSort( array, index + 1 );
+        }
+    }
+}
