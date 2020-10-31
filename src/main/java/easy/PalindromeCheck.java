8c2a3d6a344d975b14bad3ff881aa1f4465223af
Ricardo Gomes <ipsiart@gmail.com>
2020-10-31 17:05:26 +0000

    Palindrome Check

---
+++ src/main/java/easy/PalindromeCheck.java
@@ -0,0 +1,9 @@
+package easy;
+
+public class PalindromeCheck {
+    public static boolean isPalindrome(String str) {
+        // Write your code here.
+        return new StringBuilder( str ).reverse().toString()
+                .equals( str );
+    }
+}
