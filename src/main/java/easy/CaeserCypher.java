76a462a9883b9b3d873d45e993ad47ca08d39949
Ricardo Gomes <ipsiart@gmail.com>
2020-11-01 13:54:20 +0000

    Caesar Cypher

---
+++ src/main/java/easy/CaeserCypher.java
@@ -0,0 +1,20 @@
+package easy;
+
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+public class CaeserCypher {
+    public static String caesarCypherEncryptor(String str, int key) {
+        Function<Character, Character> encrypt = c -> {
+          int newValue = ((c - 'a') + key) % 26;
+          return (char) (newValue + 'a');
+        };
+
+        // Write your code here.
+        return IntStream.range( 0, str.length() ).boxed()
+                .map( str::charAt )
+                .map( encrypt )
+                .map( String::valueOf )
+                .reduce( "", String::concat );
+    }
+}
