35fc05c8f0216762ca5a794e4e5cbf3f527e4e35
Ricardo Gomes <ipsiart@gmail.com>
2020-11-02 16:36:27 +0000

    MonotonicArray

---
+++ src/main/java/mid/MonotonicArray.java
@@ -0,0 +1,21 @@
+package mid;
+
+import com.sun.tools.corba.se.idl.toJavaPortable.InterfaceGen;
+import sun.jvm.hotspot.jdi.IntegerTypeImpl;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class MonotonicArray {
+    public static boolean isMonotonic(int[] array) {
+        List<Integer> ascSortedList = Arrays.stream( array ).boxed()
+                        .sorted().collect( Collectors.toList());
+        List<Integer> desSortedList = Arrays.stream( array ).boxed()
+                .sorted((a, b) -> - a.compareTo( b )).collect( Collectors.toList());
+
+        return IntStream.range( 0, array.length ).allMatch( i -> ascSortedList.get( i ) == array[i] )
+                || IntStream.range( 0, array.length ).allMatch( i -> desSortedList.get( i ) == array[i] );
+    }
+}
