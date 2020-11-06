17e86b3810e7f111da106d335ae88815680ad119
Ricardo Gomes <ipsiart@gmail.com>
2020-11-06 19:50:54 +0000

    Invert Binart Tree

---
+++ src/main/java/mid/InvertBinaryTree.java
@@ -0,0 +1,28 @@
+package mid;
+
+import java.util.Objects;
+
+public class InvertBinaryTree {
+    public static void invertBinaryTree(BinaryTree tree) {
+        // Write your code here.
+        if (Objects.nonNull(tree)) {
+            BinaryTree left = tree.left;
+            BinaryTree right = tree.right;
+
+            tree.left = right;
+            tree.right = left;
+            invertBinaryTree( right );
+            invertBinaryTree( left );
+        }
+    }
+
+    static class BinaryTree {
+        public int value;
+        public BinaryTree left;
+        public BinaryTree right;
+
+        public BinaryTree(int value) {
+            this.value = value;
+        }
+    }
+}
