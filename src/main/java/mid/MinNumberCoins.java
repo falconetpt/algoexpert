c215a6059534f301995d4d653d4bd8d543104ea8
Ricardo Gomes <ipsiart@gmail.com>
2020-11-08 15:28:10 +0000

    Minimum number of coins

---
+++ src/main/java/mid/MinNumberCoins.java
@@ -0,0 +1,21 @@
+package mid;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MinNumberCoins {
+    public static int minNumberOfCoinsForChange(int n, int[] denoms) {
+        // Write your code here.
+        int[] result = new int[n + 1];
+        Arrays.fill( result, n + 1 );
+        result[0] = 0;
+
+        for (int coin: denoms) {
+            for (int amount = coin; amount <= n; amount++) {
+                result[amount] = Math.min( result[amount], 1 + result[amount - coin] );
+            }
+        }
+
+        return result[n] < n + 1 ? result[n] : -1;
+    }
+}
