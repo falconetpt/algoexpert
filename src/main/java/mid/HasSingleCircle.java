c937a1f7c786cfed94c0c5ba8da54cfbe7191ec2
Ricardo Gomes <ipsiart@gmail.com>
2020-11-11 11:38:55 +0000

    HasSingleCircle

---
+++ src/main/java/mid/HasSingleCircle.java
@@ -0,0 +1,15 @@
+package mid;
+
+public class HasSingleCircle {
+    public static boolean hasSingleCycle(int[] array) {
+        // Write your code here.
+        int nextArray = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (i > 0 && nextArray == 0) return false;
+            nextArray = (nextArray + array[nextArray]) % array.length;
+            nextArray = nextArray >= 0 ? nextArray : nextArray + array.length;
+        }
+
+        return nextArray == 0;
+    }
+}
