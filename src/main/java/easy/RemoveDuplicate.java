b2ebe060a851a594fd035ac9a8f53e66a37b9fc6
Ricardo Gomes <ipsiart@gmail.com>
2021-02-02 23:17:10 +0000

    remove duplicates

---
+++ src/main/java/easy/RemoveDuplicate.java
@@ -0,0 +1,39 @@
+package easy;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public class RemoveDuplicate {
+    // This is an input class. Do not edit.
+    public static class LinkedList {
+        public int value;
+        public LinkedList next;
+
+        public LinkedList(int value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    public LinkedList removeDuplicatesFromLinkedList(LinkedList linkedList) {
+        removeDuplicates(linkedList);
+
+        return linkedList;
+    }
+
+    private int removeDuplicates(LinkedList linkedList) {
+        if (Objects.nonNull(linkedList)) {
+            Optional.ofNullable( linkedList )
+                    .map( l -> l.next )
+                    .filter( l -> l.value == linkedList.value )
+                    .map( l -> {
+                        linkedList.next = l.next;
+                        removeDuplicates( linkedList );
+                        return 1;
+                    })
+                    .orElseGet( () -> removeDuplicates( linkedList.next ) );
+        }
+
+        return 1;
+    }
+}
